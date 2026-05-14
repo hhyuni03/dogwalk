fe694ed4c5ca879f2194fe76b44541641f8cc1b1
정회윤 <uni030822@gmail.com>
2026-05-14 21:28:55 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/example/dogwalk/DogwalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogwalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogwalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogwalkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/dogwalk/user/domain/User.java
@@ -0,0 +1,11 @@
+package com.example.dogwalk.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+public class User {
+}
