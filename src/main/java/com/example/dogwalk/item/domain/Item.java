3a7e6052ebe694d6f497a47359d5fdaa99af538f
정회윤 <uni030822@gmail.com>
2026-05-30 21:48:01 +0900

    feat: item, inventoryItem 엔티티 작성

---
+++ src/main/java/com/example/dogwalk/item/domain/Item.java
@@ -0,0 +1,4 @@
+package com.example.dogwalk.item.domain;
+
+public class Item {
+}
--- src/main/java/com/example/dogwalk/walk/service/WalkService.java
+++ src/main/java/com/example/dogwalk/walk/service/WalkService.java
@@ -10,6 +10,7 @@ import com.example.dogwalk.dog.domain.Dog;
 import com.example.dogwalk.dog.repository.DogRepository;
 import com.example.dogwalk.walk.domain.WalkSession;
 import com.example.dogwalk.walk.domain.WalkStatus;
+import com.example.dogwalk.walk.domain.VerificationStatus;
 import com.example.dogwalk.walk.dto.WalkEndRequest;
 import com.example.dogwalk.walk.dto.WalkStartRequest;
 import com.example.dogwalk.walk.repository.WalkRepository;
@@ -59,21 +60,17 @@ public class WalkService {
         User user = walkSession.getUser();
         walkSession.endWalk(request.getEndLat(), request.getEndLng(), request.getTotalDistanceMeters());
 
-        // 경험치 (Exp) 보상 로직
-        int photoExp = 0;
-        if(request.isPhotoVerified()){
-            walkSession.verifyPhoto(request.getEndPhotoUrl());
-            photoExp = 100;
-        }
+        // 경경치 획득
         int walkMinutes = walkSession.getTotalDurationSeconds() / 60;
         Double walkMeters = walkSession.getTotalDistanceMeters();
         int minExp = walkMinutes * 5; // 시간 보상
         int meterExp = (int)(walkMeters/100) * 10; // 거리 보상
+        int photoExp = verifyPhotoAndGetExp(walkSession, request.getEndPhotoUrl()); // 사진 보상
         int totalExp = minExp + meterExp + photoExp;
         user.addExp(totalExp);
 
+        // 포인트 획득
         user.addPoint(walkSession.getEarnedPoint());
-
         PointTransaction pointTransaction = PointTransaction.builder()
                 .user(user)
                 .transactionType(TransactionType.EARN)
@@ -81,5 +78,24 @@ public class WalkService {
                 .amount(walkSession.getEarnedPoint())
                 .build();
         pointTransactionRepository.save(pointTransaction);
+    }
+
+    // 산책 인증 사진이 유효한지 판단
+    private int verifyPhotoAndGetExp(WalkSession walkSession, String photoUrl){
+        if(photoUrl == null || photoUrl.trim().isEmpty()){
+            walkSession.updateVerification(VerificationStatus.NONE, 0);
+            return 0;
+        }
+
+        int randomScore = (int)(Math.random() * 101);
+
+        if (randomScore>=80) {
+            walkSession.updateVerification(VerificationStatus.SUCCESS, randomScore);
+            return 100;
+        }
+        else {
+            walkSession.updateVerification(VerificationStatus.FAILED, randomScore);
+            return 0;
+        }
     }
 }
